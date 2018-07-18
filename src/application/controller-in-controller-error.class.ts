578f75504e99fe10223109d10509cba38f127aaf
Mindsers <dev@nathanaelcherrier.com>
2018-07-18 23:18:13 +0200

    Fix import

--- src/application/controller-in-controller-error.class.ts
+++ src/application/controller-in-controller-error.class.ts
@@ -1,5 +1,5 @@
 import { Controller } from '../controller/controller.class'
-import { InjectionClass } from '../injector/Injection-class.interface'
+import { InjectionClass } from '../injector/injection-class.interface'
 
 export class ControllerInControllerError<C extends Controller, P extends Controller> extends Error {
   constructor(controller: InjectionClass<C>, parentController: InjectionClass<P>) {
