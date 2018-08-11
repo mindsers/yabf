dcb97eff996bf745939a746e5c6b0baae4cc0706
Mindsers <dev@nathanaelcherrier.com>
2018-08-11 17:07:51 +0200

    Change Controller as an Abstract class

--- src/controller/controller.class.ts
+++ src/controller/controller.class.ts
@@ -3,7 +3,7 @@ import { IRoute } from '../router/route.interface'
 import { IAction } from './action.interface'
 import { IController } from './controller.interface'
 
-export class Controller implements IController {
+export abstract class Controller implements IController {
   get routes(): IRoute[] {
     return []
   }
