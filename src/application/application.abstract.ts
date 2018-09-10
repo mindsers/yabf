0374f8c5bb31f576a77bf94db63c59fbad08e320
Mindsers <dev@nathanaelcherrier.com>
2018-09-10 22:28:18 +0200

    Check if child classes implements createInstance static method.

--- src/application/application.abstract.ts
+++ src/application/application.abstract.ts
@@ -4,7 +4,16 @@ import { InjectionType } from '../injector/injection-type.interface'
 import { InjectorService } from '../injector/injector.class'
 
 export abstract class Application {
-  constructor(protected injectorService: InjectorService) {}
+  constructor(protected injectorService: InjectorService) {
+    const constructor = this.constructor as { createInstance?: any; name: string }
+
+    if (constructor.createInstance == null || typeof constructor.createInstance !== 'function') {
+      console.warn(
+        `The static factory method "createInstance" haven't been created in class ${constructor.name}.`,
+        `This behavior will be depracated and removed in future version.`,
+      )
+    }
+  }
 
   provide<C>(className: InjectionType<C>): void
   provide<C>(className: InjectionClass<C>, dependencies?: InjectionSelector<any>[]): void
