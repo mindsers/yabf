04995ea3474b040cb2ef6e2dec1836c46b09261b
Mindsers <dev@nathanaelcherrier.com>
2018-09-10 21:22:06 +0200

    Rename file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './application/application.class'
 export * from './application/app-config.interface'
-export * from './application/base-application.abstract'
+export * from './application/application.abstract'
 
 export * from './controller/controller.class'
 
